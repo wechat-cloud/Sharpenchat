aaef9e1dd3754b320c33532798674c5cc7757415
Lu Rongkai <lurongkai@gmail.com>
2017-01-18 14:23:50 +0800

    add container service

---
+++ src/Sharpenchat.Core/Container/IRegistry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sharpenchat.Core.Container
+{
+    public interface IRegistry
+    {
+        TInterface Resolve<TInterface>();
+    }
+}
--- src/Sharpenchat.Core/Serialization/IXmlSerializer.cs
+++ src/Sharpenchat.Core/Serialization/IXmlSerializer.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Sharpenchat.Core.Serialization
 {
-    interface IXmlSerializer
+    public interface IXmlSerializer
     {
+        Task SerializeAsync<T>(T target, TextWriter writer);
+        Task<T> DeserializeAsync<T>(TextReader reader);
     }
 }
