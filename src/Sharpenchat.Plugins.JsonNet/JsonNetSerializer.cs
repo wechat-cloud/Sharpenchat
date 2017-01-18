98c723538b4117d661824d10b66be536d6131ff9
Lu Rongkai <lurongkai@gmail.com>
2017-01-18 14:37:13 +0800

    newtonsoft json

--- src/Sharpenchat.Plugins.JsonNet/JsonNetSerializer.cs
+++ src/Sharpenchat.Plugins.JsonNet/JsonNetSerializer.cs
@@ -2,17 +2,28 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Sharpenchat.Plugins.JsonNet
 {
     public class JsonNetSerializer : IJsonSerializer
     {
+        private readonly JsonSerializer _serializer;
+        public JsonNetSerializer()
+        {
+            _serializer = new JsonSerializer();
+        }
         public async Task<T> DeserializeAsync<T>(TextReader reader) {
-            throw new NotImplementedException();
+            var jsonTextReader = new JsonTextReader(reader);
+            var result = _serializer.Deserialize<T>(jsonTextReader);
+            return await Task.FromResult(result);
         }
 
         public async Task SerializeAsync<T>(T source, TextWriter writer) {
-            throw new NotImplementedException();
+            var jsonTextWriter = new JsonTextWriter(writer);
+            await Task.Run(() => {
+                _serializer.Serialize(jsonTextWriter, source);
+            });
         }
     }
 }
